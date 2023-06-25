e74c9ff85713c1e44109b0ef96ead7ab3cfba765
samane-92 <131759514+samane-92@users.noreply.github.com>
2023-06-25 20:11:59 +0430

    remove unnecessary files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1></h1>
+        <footer>
+          this is an{" "}
+          <a
+            href="https://github.com/samane-92/Plus-Project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced-code
+          </a>{" "}
+          was written by Samaneh R. on{" "}
+          <a href="https://github.com/" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </footer>
+        
+      </div>
     </div>
   );
 }
